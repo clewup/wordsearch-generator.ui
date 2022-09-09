eca8d512d04986d881238c389a7ae19990034ff5
clew <hello@clew.codes>
2022-09-09 21:27:30 +0100

    Basic logic and website structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import {
+  Box,
+  Text,
+  Input,
+  Wrap,
+  WrapItem,
+  Center,
+  Container,
+  Heading,
+  SimpleGrid,
+  Skeleton,
+  Button,
+} from "@chakra-ui/react";
 
 function App() {
+  const [loaded, setLoaded] = useState(true);
+  const [chosenWord, setChosenWord] = useState("");
+
+  const maximumLetters = 169; // has to return a solid number on square root.
+  const columns = Math.sqrt(maximumLetters);
+  const letterSize = 4;
+  const letterSizeVw = `${letterSize}vw`;
+  const totalSize = columns * letterSize;
+  const totalSizeVw = `${totalSize}vw`;
+
+  const LetterGenerator = () => {
+    const characters = "abcdefghijklmnopqrstuvwxyz";
+    const charactersLength = characters.length;
+    const randomLetter = characters.charAt(
+      Math.floor(Math.random() * charactersLength)
+    );
+    return randomLetter;
+  };
+
+  const WordGenerator = () => {};
+  WordGenerator();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Heading margin="1rem 0" textAlign="center">
+        Wordsearch Generator
+      </Heading>
+      <Box width="30vw" textAlign="center" margin="0 auto">
+        <Text>Enter a word!</Text>
+        <Box display="flex" gap="1rem" alignItems="center" margin="1rem">
+          <Input
+            placeholder=""
+            size="lg"
+            value={chosenWord}
+            onChange={(e) => setChosenWord(e.target.value)}
+          />
+          <Button colorScheme="green">Add!</Button>
+        </Box>
+      </Box>
+      <Box width="40vw" margin="0 auto"></Box>
+      <Box
+        width={totalSizeVw}
+        height={totalSizeVw}
+        background="background.primary"
+        margin="1rem auto"
+        border="1px solid black"
+      >
+        <Skeleton isLoaded={loaded} width={totalSizeVw} height={totalSizeVw}>
+          <SimpleGrid columns={columns}>
+            {[...Array(maximumLetters)].map((e, i) => (
+              <Box
+                key={i}
+                id="letterSquare"
+                width={letterSizeVw}
+                height={letterSizeVw}
+                textAlign="center"
+                lineHeight={letterSizeVw}
+                fontSize="1.2rem"
+              >
+                {LetterGenerator()}
+              </Box>
+            ))}
+          </SimpleGrid>
+        </Skeleton>
+      </Box>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      primary: "#f7fafc",
+      secondary: "#1a202c",
+    },
+    background: {
+      primary: "#f7fafc",
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
